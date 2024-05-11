660c09c38513faa232bd2eaad6e6bcf6de574b3a
mugen <1870282173@qq.com>
2024-05-11 09:47:58 +0800

    项目初始结构创建

---
+++ gmall-product/src/test/java/com/mug/gmall/product/GmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mug.gmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gmall-ware/src/test/java/com/mug/gmall/ware/GmallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mug.gmall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
